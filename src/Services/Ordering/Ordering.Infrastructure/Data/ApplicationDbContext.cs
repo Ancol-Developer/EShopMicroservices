cc998caec10502f33246f6e6b49bd8fcf1cb53d1
Ancol-Developer <trandieu026@gmail.com>
2025-09-28 13:03:02 +0700

    Configuration Entities

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    DbSet<Customer> Customers => Set<Customer>();
+    DbSet<Product> Products => Set<Product>();
+    DbSet<Order> Orders => Set<Order>();
+    DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
