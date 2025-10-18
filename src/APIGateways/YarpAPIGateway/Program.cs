002fd595a3ec7e53e6f2d6a757b8f6c171006a50
Ancol-developer <trandieu026@gmail.com>
2025-10-19 00:10:30 +0700

    Add API Gateway

---
+++ src/APIGateways/YarpAPIGateway/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service to container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapReverseProxy();
+
+app.Run();
