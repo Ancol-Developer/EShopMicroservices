ec07009a8e28c6db708e37e900458ba483f62da7
Ancol-Developer <trandieu026@gmail.com>
2025-11-01 15:27:42 +0700

    Develop Cart Page

--- src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Shopping.Web.Pages
 {
-    public class CartModel : PageModel
+    public class CartModel(IBasketService basketService, ILogger<CartModel> logger)
+        : PageModel
     {
-        public void OnGet()
+        public ShoppingCartModel Cart { get; set; } = new();
+        public async Task<IActionResult> OnGetAsync()
         {
+            Cart = await basketService.LoadUserBasket();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Remove from cart button clicked");
+            Cart = await basketService.LoadUserBasket();
+            Cart.Items.RemoveAll(x => x.ProductId == productId);
+            await basketService.StoreBasket(new StoreBasketRequest(Cart));
+
+            return RedirectToPage();
         }
     }
 }
